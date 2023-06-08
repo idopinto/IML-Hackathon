0008289229892c5bc6899ffe0a99d0dc1d450311
michael_sv <michael.svitlizky@mail.huji.ac.il>
2023-06-08 15:36:28 +0300

    copied utils

--- task_1/code/hackathon_code/Utils/utils.py
+++ task_1/code/hackathon_code/Utils/utils.py
@@ -1,7 +1,10 @@
+from sklearn.ensemble import RandomForestClassifier
+
 from . import pp
 from ..Base import baseline
 import pandas as pd
 from sklearn.metrics import precision_score, recall_score
+from sklearn.metrics import f1_score
 
 
 def load_data(filename):
@@ -9,18 +12,13 @@ def load_data(filename):
                        "checkout_date", "hotel_live_date", "cancellation_datetime"]
     df = pd.read_csv(filename, parse_dates=dates_to_parse)
     return df
-def pipeline(df):
-
-    # preprocessing
-    X, y = pp.preprocess(df)
-
+def pipeline(X,y):
     # fit
-    model = baseline.BaseEstimator().fit(X, y)
-
+    base = baseline.BaseEstimator().fit(X, y)
     # calculate loss
-    f1 = model.loss(X, y)
+    f1 = base.loss(X, y)
 
     # Calculate precision and recall
-    precision, recall = model.get_recall_precision(X, y)
+    precision, recall = base.get_recall_precision(X, y)
 
     return f1, precision, recall
