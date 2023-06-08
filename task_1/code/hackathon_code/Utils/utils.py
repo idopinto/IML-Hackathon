fff19544a214f77a9bde945b929635f577932ff5
Israel Ben David <israel1011998@gmail.com>
2023-06-08 20:16:08 +0300

    new html

--- task_1/code/hackathon_code/Utils/utils.py
+++ task_1/code/hackathon_code/Utils/utils.py
@@ -77,5 +77,29 @@ def cancellation_cost(cancellation_policy, cancellation_date, checkin_date, orde
 
 
 
+def parse_policy(policy_code, num_nights):
+    policies = policy_code.split("_")
+    parsed_policy = [-1, -1, -1, -1, -1]  # Initialize the list with -1
+
+    for i, policy in enumerate(policies):
+        if policy == '':
+            continue
+        if "D" not in policy:
+            policy_type = policy[-1]  # Get the last character of policy, which is 'N' or 'P'
+            charge = int(policy.split(policy_type)[0].split(' ')[-1])  # Get the charge value before 'N' or 'P'
+            if policy_type == 'P':
+                parsed_policy[4] = charge
+            else:  # policy_type == 'N'
+                parsed_policy[4] = (charge / num_nights) * 100
+        else:
+            days, charge, policy_type = policy.split('D')[0], policy.split('D')[1][:-1], policy.split('D')[1][-1]
+            if policy_type == 'P':
+                parsed_policy[i*2] = int(days)
+                parsed_policy[i*2 + 1] = int(charge)
+            else:  # policy_type == 'N'
+                parsed_policy[i*2] = int(days)
+                parsed_policy[i*2 + 1] = (int(charge) / num_nights) * 100
+
+    return parsed_policy
 
 
--- task_1/code/hackathon_code/playgrounds/policy_checker.py
+++ task_1/code/hackathon_code/playgrounds/policy_checker.py
@@ -1,15 +1,26 @@
-from task_1.code.hackathon_code.Utils.utils import cancellation_cost
+from task_1.code.hackathon_code.Utils.utils import parse_policy
 
-# Assume an order cost of $500 for 5 nights
+assert parse_policy("60D1N_30D100P_100P", 5) == [60, 20, 30, 100, 100]
+assert parse_policy("60D1N_30D100P", 5) == [60, 20, 30, 100, -1]
+assert parse_policy("60D100P_30D1N", 5) == [60, 100, 30, 20, -1]
+assert parse_policy("60D100P_1N", 5) == [60, 100, -1, -1, 20]
+assert parse_policy("1N", 5) == [-1, -1, -1, -1, 20]
+assert parse_policy("60D1N", 5) == [60, 20, -1, -1, -1]
+assert parse_policy("", 5) == [-1, -1, -1, -1, -1]
 
-print(cancellation_cost("365D100P_100P", "2023-05-10", "2023-06-10", 1000, 5))  # Expected output: 1000
-print(cancellation_cost("365D100P_100P", "2023-04-10", "2023-06-10", 1000, 5))  # Expected output: 1000
-print(cancellation_cost("365D1N_1N", "2023-05-10", "2023-06-10", 1000, 5))  # Expected output: 200
-print(cancellation_cost("365D1N_1N", "2023-04-10", "2023-06-10", 1000, 5))  # Expected output: 200
-print(cancellation_cost("30D50P_1N", "2023-05-11", "2023-06-10", 1000, 5))  # Expected output: 500
-print(cancellation_cost("30D50P_1N", "2023-04-10", "2023-06-10", 1000, 5))  # Expected output: 0
-print(cancellation_cost("60D1N_30D100P_100P", "2023-05-11", "2023-06-10", 1000, 5))  # Expected output: 1000
-print(cancellation_cost("60D1N_30D100P_100P", "2023-04-11", "2023-06-10", 1000, 5))  # Expected output: 200
-print(cancellation_cost("100P", "2023-06-10", "2023-06-10", 1000, 5))  # Expected output: 1000
-print(cancellation_cost("1N", "2023-06-10", "2023-06-10", 1000, 5))  # Expected output: 200
+assert parse_policy("90D2N_15D50P_100P", 10) == [90, 20, 15, 50, 100]
+assert parse_policy("60D50P_2N", 4) == [60, 50, -1, -1, 50]
+assert parse_policy("2N", 4) == [-1, -1, -1, -1, 50]
+assert parse_policy("90D2N", 10) == [90, 20, -1, -1, -1]
+assert parse_policy("30D100P", 5) == [30, 100, -1, -1, -1]
+assert parse_policy("60D50P_15D2N_1N", 5) == [60, 50, 15, 40, 20]
+assert parse_policy("90D100P_30D2N", 10) == [90, 100, 30, 20, -1]
+assert parse_policy("45D100P_3N", 3) == [45, 100, -1, -1, 100]
+assert parse_policy("3N", 3) == [-1, -1, -1, -1, 100]
+assert parse_policy("90D3N", 10) == [90, 30, -1, -1, -1]
+assert parse_policy("30D100P_1N", 5) == [30, 100, -1, -1, 20]
+assert parse_policy("60D2N_15D50P_1N", 5) == [60, 40, 15, 50, 20]
+assert parse_policy("90D100P_30D3N", 10) == [90, 100, 30, 30, -1]
+
+
 
