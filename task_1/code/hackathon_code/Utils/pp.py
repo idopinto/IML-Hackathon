78d08399c6cb816fd252b5ae36c9b772bec15413
matan <matan.sheinberg@mail.huji.ac.il>
2023-06-08 16:28:52 +0300

    add pp

--- task_1/code/hackathon_code/Utils/pp.py
+++ task_1/code/hackathon_code/Utils/pp.py
@@ -6,9 +6,9 @@ from sklearn.preprocessing import LabelEncoder
 
 def preprocess(df):
     encoder = LabelEncoder()
-    df['language'] = encoder.fit_transform(df['language'])
-    df['hotel_country_code'] = encoder.fit_transform(df['hotel_country_code'])
-    df["accommadation_type_name"] = encoder.fit_transform(df["accommadation_type_name"])
+    df['language'] = encoder.fit_transform(df['language']).astype(float)
+    df['hotel_country_code'] = encoder.fit_transform(df['hotel_country_code']).astype(float)
+    df["accommadation_type_name"] = encoder.fit_transform(df["accommadation_type_name"]).astype(float)
 
     df['request_nonesmoke'] = df['request_nonesmoke'].apply(lambda x: x if (x == 0) or (x == 1) else -1)
     df['request_airport'] = df['request_airport'].apply(lambda x: x if (x == 0) or (x == 1) else -1)
@@ -20,14 +20,14 @@ def preprocess(df):
     df['hotel_brand_code'] = df['hotel_brand_code'].apply(lambda x: x if x >= 0 else -1)
     df['hotel_chain_code'] = df['hotel_chain_code'].apply(lambda x: x if x >= 0 else -1)
 
-    df["has_request"] = (df['request_nonesmoke'] == 1) or (df['request_nonesmoke'] == 1) or \
-                        (df['request_nonesmoke'] == 1) or (df['request_airport'] == 1) or \
-                        (df['request_latecheckin'] == 1) or (df['request_highfloor'] == 1) or \
-                        (df['request_twinbeds'] == 1) or (df['request_earlycheckin'] == 1) or \
-                        (df['request_largebed'] == 1)
+    # df["has_request"] = (df['request_nonesmoke'] == 1) or (df['request_airport'] == 1) or \
+    #                     (df['request_latecheckin'] == 1) or (df['request_highfloor'] == 1) or \
+    #                     (df['request_twinbeds'] == 1) or (df['request_earlycheckin'] == 1) or \
+    #                     (df['request_largebed'] == 1)
 
     df["did_cancel"] = ~df["cancellation_datetime"].isna()
-    df["distance_booking_checkin"] = ((df["checkin_date"] - df["booking_datetime"]) / pd.Timedelta(days=1)).astype(float)
+    df["distance_booking_checkin"] = ((df["checkin_date"] - df["booking_datetime"]) / pd.Timedelta(days=1)).astype(
+        float)
     df["amount_guests"] = df["no_of_adults"] + df["no_of_children"]
     df["amount_nights"] = ((df["checkout_date"] - df["checkin_date"]) / pd.Timedelta(days=1)).astype(float)
     df["checkin_date"] = df["checkin_date"].dt.dayofyear
